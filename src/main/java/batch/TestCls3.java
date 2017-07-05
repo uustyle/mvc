3c381804afc1834f0f5329bfb9feb4aed26c9617
荒川由貴宏 <yukihiro.arakawa@gmail.com>
2017-07-05 23:53:07 +0900

    a

--- src/main/java/batch/TestCls3.java
+++ src/main/java/batch/TestCls3.java
@@ -1,8 +1,8 @@
 package batch;
 
 import java.io.File;
+import java.nio.ByteBuffer;
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.LinkedHashMap;
 import java.util.List;
 
@@ -12,6 +12,35 @@ import org.apache.commons.lang3.ArrayUtils;
 public class TestCls3 {
 
 	public static void main(String[] args) throws Exception{
+
+		//バッファーインスタンスを生成する
+		ByteBuffer bb = ByteBuffer.allocate(10);
+		bb.put((byte)0x01);
+		bb.put((byte)0x02);
+		bb.put((byte)0x03);
+
+
+		bb.put((byte)0x04);
+		bb.put((byte)0x05);
+		bb.put((byte)0x06);
+		bb.put((byte)0x07);
+		bb.put((byte)0x08);
+		bb.put((byte)0x09);
+		bb.put((byte)0x0a);
+
+		byte[] bytedt = bb.array();
+	    for (int i=0;i<bytedt.length;++i) {
+	    	System.out.println("16進数表現2桁:"+String.format("%02x", bytedt[i]));
+	    }
+
+		byte[] bytedt2 = new byte[2];
+		bb.position(0);
+		bb.get(bytedt2,0,2);
+	    for (int i=0;i<bytedt2.length;++i) {
+	    	System.out.println("16進数表現2桁:"+String.format("%02x", bytedt2[i]));
+	    }
+
+
 
 		List<LinkedHashMap<String, FldDto>> fldList = new ArrayList<LinkedHashMap<String, FldDto>>();
 		LinkedHashMap<String, FldDto> fldMap= new LinkedHashMap<String, FldDto>();
@@ -33,9 +62,9 @@ public class TestCls3 {
 //		b[1] = "22";
 //		dto2.setValue(b);
 
-		Object bb[] = (Object[])dto2.getValue();
-		bb[0] = "11";
-		bb[1] = "22";
+//		Object bb[] = (Object[])dto2.getValue();
+//		bb[0] = "11";
+//		bb[1] = "22";
 
 
 		System.out.println(ArrayUtils.toString(dto.getValue()));
